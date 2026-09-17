8b94b08078863a493f0a66796b4b90a8329e3869
verinikat <github.debunk664@passmail.com>
2026-09-17 11:03:47 +0800

    Add OnlyEncodes+ (OE+) (#34)
    * add onlyencodes
    
    * attrib comment

--- quiCKIE.user.js
+++ quiCKIE.user.js
@@ -227,6 +227,12 @@
 // @match   https://oldtoons.world/playlists/*
 // @match   https://oldtoons.world/torrents*
 
+// @match   https://onlyencodes.cc/
+// @match   https://onlyencodes.cc/*/bookmarks
+// @match   https://onlyencodes.cc/playlists/*
+// @match   https://onlyencodes.cc/torrents*
+
+
 // @match   https://orpheus.network/artist.php?id=*
 // @match   https://orpheus.network/bookmarks.php*
 // @match   https://orpheus.network/collages.php?id=*
@@ -603,6 +609,12 @@ const settingsPanelTrackers = [
         trackerName: 'Oldtoons',
         homepageURL: 'https://oldtoons.world',
         primaryDomain: 'oldtoons',
+    },
+
+    {
+        trackerName: 'OnlyEncodes', // @verinikat
+        homepageURL: 'https://onlyencodes.cc',
+        primaryDomain: 'onlyencodes',
     },
 
     {
@@ -1706,6 +1718,12 @@ if ( primaryDomain == 'animebytes' ) {
     // Browse | Details | Homepage | Playlists | Similar
 
     unit3dTrackerHandler('a[href^="https://oldtoons.world/torrents/download/"]')
+
+} else if ( primaryDomain == 'onlyencodes' ) {
+    // ----------------------------------- OnlyEncodes -----------------------------------
+    // Browse | Details | Homepage | Playlists | Similar
+
+    unit3dTrackerHandler('a[href^="https://onlyencodes.cc/torrents/download/"]')
 
 } else if ( primaryDomain == 'orpheus' ) {
     // ----------------------------------- Orpheus -----------------------------------
